b3770dad73670b22ea771ea16ba72c0a9361443b
samirkhan-947 <samirrkhan947@gmail.com>
2023-01-29 18:42:53 +0530

    add-migration with database confiquartions

---
+++ HotelListing/Data/DataBaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data
+{
+    public class DataBaseContext:DbContext
+    {
+        public DataBaseContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Country> countries { get; set; }
+        public DbSet<Hotel> hotels { get; set; }
+    }
+}
